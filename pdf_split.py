36ca57f1f5ac76fbc504360c03e7490685f60fa9
Simon Park <juhyunp@princeton.edu>
2023-02-04 16:27:02 -0500

    resolved known errata as of 02/04/2023 and updated the pdf_split.py file to no longer include deprecated code

--- pdf_split.py
+++ pdf_split.py
@@ -2,19 +2,19 @@
 NUM_CHAPTERS = 20
 PART_MARKERS = ['I', 'II', 'III', 'IV', 'V', 'VI']
 
-from PyPDF2 import PdfFileReader, PdfFileWriter
-pdf = PdfFileReader("files/COS324_Course_Notes.pdf")
+from PyPDF2 import PdfReader, PdfWriter
+pdf_reader = PdfReader("files/COS324_Course_Notes.pdf")
 
 ## Find the page number where the Table of Contents starts
 toc_page_no = 0
-while toc_page_no < pdf.getNumPages():
-    lines = pdf.pages[toc_page_no].extract_text().split('\n')
+while toc_page_no < len(pdf_reader.pages):
+    lines = pdf_reader.pages[toc_page_no].extract_text().split('\n')
     if lines[0] == 'Contents':
         break
     toc_page_no += 1
 
 ## Find the start page of each chapter and each part
-lines = pdf.pages[toc_page_no].extract_text().split('\n')
+lines = pdf_reader.pages[toc_page_no].extract_text().split('\n')
 line_no = 0
 chap_no = 1
 page_split_markers = []
@@ -36,18 +36,18 @@ while chap_no <= NUM_CHAPTERS:
 
     if line_no == len(lines):
         toc_page_no += 1
-        lines = pdf.pages[toc_page_no].extract_text().split('\n')
+        lines = pdf_reader.pages[toc_page_no].extract_text().split('\n')
         line_no = 0
 
-page_split_markers[-1].append(pdf.getNumPages() + 1)
+page_split_markers[-1].append(len(pdf_reader.pages) + 1)
 
 ## Save files for individual chapters and parts
 part_no = 1
 chap_no = 1
 for part in page_split_markers:
-    pdf_writer = PdfFileWriter()
+    pdf_writer = PdfWriter()
     for page in range(part[0], part[-1]):
-        pdf_writer.addPage(pdf.getPage(page - 1))
+        pdf_writer.add_page(pdf_reader.pages[page - 1])
     
     outputFilename = "files/part{}.pdf".format(part_no)
     with open(outputFilename, "wb") as out:
@@ -57,9 +57,9 @@ for part in page_split_markers:
     print("created", outputFilename)
 
     for i in range(1, len(part) - 1):
-        pdf_writer = PdfFileWriter()
+        pdf_writer = PdfWriter()
         for page in range(part[i], part[i + 1]):
-            pdf_writer.addPage(pdf.getPage(page - 1))
+            pdf_writer.add_page(pdf_reader.pages[page - 1])
 
         outputFilename = "files/ch{}.pdf".format(chap_no)
         with open(outputFilename, "wb") as out:
